5c1bcfb40725f1bfd183b3f63dd61e858aeb093c
TheBlackPharaon <59323204+TheBlackPharaon@users.noreply.github.com>
2022-01-26 18:13:56 +0000

    26.01.2022 2nd update

--- src/main/java/esig/tg/gestionbanque/Component/CronPlanner.java
+++ src/main/java/esig/tg/gestionbanque/Component/CronPlanner.java
@@ -2,6 +2,7 @@ package esig.tg.gestionbanque.Component;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -27,55 +28,81 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @EnableScheduling
 public class CronPlanner {
 
-
-    //test
- /*   @Scheduled(fixedDelay = 1000)
-    public void scheduleFixedDelayTask() {
-        System.out.println(
-                "Fixed delay task - " + System.currentTimeMillis() / 1000);
+    @Scheduled(cron ="0 5 * * * *")
+    public void updateCsv(){
+        System.out.println("fichier csv mis à jour");
+        csvfileSave();
     }
-    @Scheduled(cron = "5 * * * * ?")
-    public void dailycheck()throws MessagingException, UnsupportedEncodingException {
-
-        System.out.println("Bonjour leve toi");
-    }
-*/
 
     @Autowired
     private JavaMailSender mailSender;
 
-
     @Scheduled(cron = "0 50 16 * * *")
-    public void sendEmail(/*String toEmail,
-                          String subject,
-                          String body*/){
+    public void sendEmail(){
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("othnielignacio@gmail.com");
         message.setTo("ananijosue08@gmail.com");
         message.setText("This is a test");
         message.setSubject("Test");
 
+        /*
+        FileSystemResource file
+                = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment("test.csv", file);
+*/
         mailSender.send(message);
 
         System.out.println("Mail sent successfully...");
     }
 
-    /*
-    //@Scheduled(cron="0/3 0 * 1/1 * ? *")
-        @Scheduled(fixedDelay = 1000)
-        public void dailycheck()throws MessagingException, UnsupportedEncodingException {
 
-            System.out.println("Bonjour leve toi");
+    public void csvfileSave(){
+
+        List<String[]> datalines = new ArrayList<>();
+        datalines.add(new String[]{"nom","prenom","age"});
+        datalines.add(new String[]{"yao","koffi","20"});
+        datalines.add(new String[]{"komi","zika","28"});
+        datalines.add(new String[]{"restga","bob","29"});
+
+
+        File CsvOutPutFile = new File("esig_test.csv");
+
+        try (PrintWriter pw = new PrintWriter(CsvOutPutFile)){
+            datalines.stream()
+                    .map(this::convertToCSV)
+                    .forEach(pw::println);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-    */
-/*
+        assertTrue(CsvOutPutFile.exists());
+    }
+
+    public String convertToCSV(String [] data){
+
+        return data !=null ? Stream.of(data)
+                .map(this::escapeSpecialCharacters)
+                .collect(Collectors.joining(";")):"";
+    }
+
+    public String escapeSpecialCharacters(String data) {
+        String escapedData = "";
+        System.out.println(data);
+        if (data != null && !(data.isEmpty())) {
+            escapedData = data.replaceAll("\\R", " ");
+            if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+                data = data.replace("\"", "\"\"");
+                escapedData = "\"" + data + "\"";
+            }
+        }
+
+        return escapedData;
+    }
+
+  /*
+
     @Autowired
     public CronPlanner(JavaMailSender javaMailSender){
         this.javaMailSender = javaMailSender;
-    }
-
-    public void dailycheck() throws MessagingException, UnsupportedEncodingException {
-        System.out.println("Bonjour, lève toi");
     }
 
     public void sendMail() throws MessagingException, UnsupportedEncodingException{
@@ -100,45 +127,5 @@ public class CronPlanner {
         javaMailSender.send(message);
         System.out.println("Message envoyé");
     }
-
-    public void csvfilesave(){
-        List<String[]> datalines = new ArrayList<>();
-        datalines.add(new String[]{"nom","prenom","age"});
-        datalines.add(new String[]{"yao","koffi","20"});
-        datalines.add(new String[]{"komi","zika","28"});
-        datalines.add(new String[]{"restga","bob","29"});
-
-        File CsvOutPutFile = new File("esig_test.csv");
-
-        try(PrintWriter pw = new PrintWriter(CsvOutPutFile)){
-            datalines.stream()
-                    .map(this::convertToCSV)
-                    .forEach(pw::println);
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-        assertTrue(CsvOutPutFile.exists());
-    }
-    public String convertToCSV(String [] data){
-
-        return data !=null ? Stream.of(data)
-                .map(this::escapeSpecialCharacters)
-                .collect(Collectors.joining(";")):"";
-    }
-
-    public String escapeSpecialCharacters(String data) {
-        String escapedData = "";
-        System.out.println(data);
-        if (data != null && !(data.isEmpty())) {
-            escapedData = data.replaceAll("\\R", " ");
-            if (data.contains(",") || data.contains("\"") || data.contains("'")) {
-                data = data.replace("\"", "\"\"");
-                escapedData = "\"" + data + "\"";
-            }
-        }
-
-        return escapedData;
-    }
 */
-
 }
